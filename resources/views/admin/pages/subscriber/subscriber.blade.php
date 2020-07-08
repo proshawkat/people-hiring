c009a521ecf27a5eba3193071957d34e80f1fa10
prshawkat <showkatalimon@gmail.com>
2020-07-08 11:06:05 +0600

    bug fixing

--- resources/views/admin/pages/subscriber/subscriber.blade.php
+++ resources/views/admin/pages/subscriber/subscriber.blade.php
@@ -4,7 +4,7 @@
 <div class="page-header row no-gutters py-4">
 	<div class="col-12 col-sm-4 text-center text-sm-left mb-0">
 	    <span class="text-uppercase page-subtitle">Overview</span>
-	    <h3 class="page-title">Contact Tables</h3>
+	    <h3 class="page-title">Subscribers Tables</h3>
 	</div>
 </div>
 
